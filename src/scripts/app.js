d81f1fb014829e94834e6889d18e1db61fd6234a
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-05-27 20:49:24 -0500

    refactor eventlisteners

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,3 @@
-// url "https://api.mapbox.com/geocoding/v5/mapbox.places/starbucks.json?bbox=-77.083056,38.908611,-76.997778,38.959167&access_token=pk.eyJ1IjoibWF5dXJwYXRlbDc4NjQ1IiwiYSI6ImNrcDVrYjh0cjF5azAyb3RhbjBnOWN6ajIifQ.MS0bHsUsb-hMZ1dABPcqHQ"
-
 const urlObj = () => {
   const accessToken = "pk.eyJ1IjoibWF5dXJwYXRlbDc4NjQ1IiwiYSI6ImNrcDdhMmhkbjBlMGMyd3FlYm14NG00cDYifQ.5WNbqYVG6bA7x6UJ9CZfNA";
   const bBox = "-97.325875, 49.766204, -96.953987, 49.99275";
@@ -12,13 +10,12 @@ const getData = async(userSearch) => {
   const url = `${baseUrl}${userSearch}.json?bbox=${bBox}&access_token=${accessToken}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 }
 
-const originsDataArray = async(userSearch) => {
+const dataArray = async(userSearch) => {
   const data = await getData(userSearch);
-  const originsArray = [];
+  const arrayOfObj = [];
   data.features.forEach(item => {
     const originsObj = {
       latitude: item.center[1],
@@ -26,88 +23,79 @@ const originsDataArray = async(userSearch) => {
       name: item.text,
       address: item.properties.address ?? '',
     }
-    originsArray.push(originsObj);
+    arrayOfObj.push(originsObj);
   });
-  return originsArray;
+  return arrayOfObj;
 }
 
-const originsHtml = (element) => {
+const html = (element) => {
+  console.log('hey');
   return `<li data-long="${element.longitude}" data-lat="${element.latitude}" class="">
       <div class="name">${element.name}</div>
       <div>${element.address}</div>
   </li>`
 }
 
-function renderOrigin(originsArray) {
+const renderOrigin = (originArray) => {
   const origins = document.querySelector('.origins');
   origins.innerHTML = '';
-  originsArray.forEach(element => {
-    const htmlString = originsHtml(element);
+  originArray.forEach(element => {
+    const htmlString = html(element);
     origins.insertAdjacentHTML('beforeend', htmlString);
   });
 }
 
-function renderDestination(originsArray) {
-  console.log('reaching');
+const renderDestination = (destinationsArray) => {
   const destinations = document.querySelector('.destinations');
   destinations.innerHTML = '';
-  originsArray.forEach(element => {
-    const htmlString = originsHtml(element);
+  destinationsArray.forEach(element => {
+    const htmlString = html(element);
     destinations.insertAdjacentHTML('beforeend', htmlString);
   });
 }
 
 const render = async(userSearch, inputName) => {
-  const originsArray = await originsDataArray(userSearch);
-  if (inputName === 'Origin') return renderOrigin(originsArray);
-  console.log('pass here')
-  if (inputName === 'Destinations') return renderDestination(originsArray);
+  const arrayOfObj = await dataArray(userSearch);
+  if (inputName === 'origins') return renderOrigin(arrayOfObj);
+  renderDestination(arrayOfObj);
 }
 
 const originForm = document.querySelector('.origin-form');
 const destinationForm = document.querySelector('.destination-form');
 const handleSelected = (e) => {
   const eventTarget = e.target;
-  removeSelected();
   if (eventTarget.nodeName === 'LI' || eventTarget.nodeName === 'DIV') {
+    removeSelected(eventTarget);
     addSelected(eventTarget);
   }
 }
-
-function addSelected(eventTarget) {
-  if (eventTarget.dataset.long) {
-    console.log(eventTarget);
-    eventTarget.classList.add('selected');
-  } else {
-    console.log(eventTarget);
-    eventTarget.parentElement.classList.toggle('selected');
+const addSelected = (eventTarget) => {
+  if (!eventTarget.dataset.long) {
+    eventTarget.parentElement.classList.add('selected'); 
   }
+  eventTarget.classList.add('selected');
 }
 
-function removeSelected() {
-  document.querySelectorAll('li').forEach(li => {
+const removeSelected = (eventTarget) => {
+  if (eventTarget.closest('ul').className === 'origins') return removeSelectedUl('origins');
+  removeSelectedUl('destinations');
+}
+
+const removeSelectedUl = (ulName) => {
+  document.querySelectorAll(`.${ulName} li`).forEach(li => {
     li.classList.remove('selected');
   });
 }
 
-const handleOriginInput = (e) => {
+const handleInputEvent = (e) => {
   e.preventDefault();
   const eventTarget = e.target;
   const userSearch = eventTarget.firstElementChild.value;
-  render(userSearch, 'Origin');
-  const origins = document.querySelector('.origins');
-  origins.addEventListener('click', handleSelected);
+  const ulName = `${eventTarget.className.slice(0, -5)}s`;
+  console.log(ulName);
+  render (userSearch, ulName);
+  document.querySelector(`.${ulName}`).addEventListener('click', handleSelected);
 }
 
-const handleDestinationInput = (e) => {
-  e.preventDefault();
-  const eventTarget = e.target;
-  const userSearch = eventTarget.firstElementChild.value;
-  console.log(userSearch);
-  render(userSearch, 'Destinations');
-  const destinations = document.querySelector('.destinations');
-  destinations.addEventListener('click', handleSelected);
-}
-
-originForm.addEventListener('submit', handleOriginInput);
-destinationForm.addEventListener('submit', handleDestinationInput);
+originForm.addEventListener('submit', handleInputEvent);
+destinationForm.addEventListener('submit', handleInputEvent);
