4bdc834be9959a527be737a16d83a2bfb25f1ca3
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-05-28 20:04:21 -0500

    create trip plans data

--- src/scripts/tripPlanner.js
+++ src/scripts/tripPlanner.js
@@ -9,29 +9,31 @@ const getCoords = (elementName) => {
   });
   return [latitude, longitude];
 }
-//https://api.winnipegtransit.com/home/api/v3/trip-planner.json?api-key=1FyswLynq7PUOTS5dzBJ&origin=geo/49.828529,-97.150745&destination=geo/49.835033,-97.149976
+
 const tripPlannerUrl = () => {
-  const baseUrl = `https://api.winnipegtransit.com/home/api/v3/trip-planner.json?`
+  const baseUrl = `https://api.winnipegtransit.com/v3/trip-planner.json?`
   const apiKey = `1FyswLynq7PUOTS5dzBJ`;
   return {baseUrl, apiKey};
 }
 
-const test = async() => {
+const getPlansData = async() => {
   const {baseUrl, apiKey} = tripPlannerUrl();
   const originCoords = getCoords(`origins`);
   const destCoords = getCoords('destinations');
-  console.log(originCoords, destCoords);
-  const url = `${baseUrl}api-key=${apiKey}&origin=geo/${originCoords[0],originCoords[1]}&destination=geo/${destCoords[0],destCoords[1]}`;
+  const url = `${baseUrl}api-key=${apiKey}&origin=geo/${originCoords[0]},${originCoords[1]}&destination=geo/${destCoords[0]},${destCoords[1]}`;
   const response = await fetch(url);
   const data = await response.json();
   console.log(data);
+  return data;
 }
+
+
 
 const planTripButton = document.querySelector('.plan-trip');
 
 const handleButton = (e) => {
   const eventTarget = e.target;
-  test();
+  getPlansData();
 }
 
 planTripButton.addEventListener('click', handleButton);
