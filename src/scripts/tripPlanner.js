4c045c3bd9c8bd276c9dc1d0ad4fc840523d7c71
mayurpatel78645 <mayurpatel78645@gmail.com>
2021-05-29 13:24:06 -0500

    refactor plansObj

--- src/scripts/tripPlanner.js
+++ src/scripts/tripPlanner.js
@@ -3,11 +3,10 @@ const getCoords = (elementName) => {
   let latitude, longitude;
   element.forEach(item => {
     if (item.classList.contains('selected')) {
-      latitude = item.dataset.lat;
-      longitude = item.dataset.long;
+      latitude = item.dataset.lat ?? handleEdgeCase('location not found');
+      longitude = item.dataset.long ?? handleEdgeCase('location not found');
     }
   });
-  if (latitude === undefined || longitude === undefined) return document.querySelector('.my-trip').innerHTML = 'location not found';
   return [latitude, longitude];
 }
 
@@ -21,13 +20,12 @@ const getPlansData = async() => {
   const {baseUrl, apiKey} = tripPlannerUrl();
   const originCoords = getCoords(`origins`);
   const destCoords = getCoords('destinations');
-  console.log(originCoords, destCoords)
   const url = `${baseUrl}api-key=${apiKey}&origin=geo/${originCoords[0]},${originCoords[1]}&destination=geo/${destCoords[0]},${destCoords[1]}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
     return data;
-  }catch(err) {document.querySelector('.my-trip').innerHTML = 'currently no buses available';}
+  }catch(err) {handleEdgeCase('Try selecting origin and destination else currently no buses available')}
 }
 
 const getIcons = () => {
@@ -37,40 +35,23 @@ const getIcons = () => {
   return {walkingIcon, rideIcon, transferIcon};
 }
 
-const renderTransitHtml = (plan) => {
+const renderTransitHtml = (plan, myTrip) => {
   const {walkingIcon, rideIcon, transferIcon} = getIcons();
-  const myTrip = document.querySelector('.my-trip');
-  myTrip.innerHTML = '';
   plan.segments.forEach(segment => {
     switch (segment.type) {
       case 'walk':
         myTrip.insertAdjacentHTML('beforeend', 
-        `
-        <li>
-          <i class="${walkingIcon}" aria-hidden="true"></i>Walk for ${segment.times.durations.total} minutes
-          to stop #${(((segment || {}).to || {}).stop || {}).key?? 'destination'} - ${(((segment || {}).to || {}).stop || {}).name?? 'destination'}
-        </li>
-        `
+        walkHtml(walkingIcon, segment)
         );
         break;
       case 'ride':
         myTrip.insertAdjacentHTML('beforeend', 
-        `
-        <li>
-          <i class="${rideIcon}" aria-hidden="true"></i>Ride the ${((segment || {}).route || {}).name?? ((segment || {}).route || {}).number} 
-          for ${segment.times.durations.total} minutes.
-        </li>
-        `
+        rideHtml(rideIcon, segment)
         );
         break;
       case 'transfer':
         myTrip.insertAdjacentHTML('beforeend', 
-        `
-        <li>
-          <i class="${transferIcon}" aria-hidden="true"></i>Transfer from stop
-          #${segment.from.stop.key} - ${segment.from.stop.name} to stop #${(segment.to.stop || {}).key} - ${(segment.to.stop || {}).name}
-        </li>
-        `
+        transferHtml(transferIcon, segment)
         );
         break;
       default:
@@ -78,49 +59,90 @@ const renderTransitHtml = (plan) => {
   });
 }
 
-const generateTripPlansArray = async() => {
+const insertSegmentHtml = (myTrip, index) =>{
+  myTrip.insertAdjacentHTML('beforeend', `
+  <h2>Route ${index === 0 ? index = 'Recommended' : index = `Alternative ${index}`}<br>
+  `);
+  return index;
+}
+
+const transferHtml = (transferIcon, segment) => {
+  const { stopName, stopNumber, fromStopNumber, fromStopName } = tripPlansObj(segment);
+  return `
+  <li>
+    <i class="${transferIcon}" aria-hidden="true"></i>
+    Transfer from stop
+    #${fromStopNumber} - ${fromStopName} to stop #${stopNumber} - ${stopName}
+  </li>
+  `;
+}
+
+const rideHtml = (rideIcon, segment) => {
+  const { duration,routeName, routNumber } = tripPlansObj(segment);
+  return `
+  <li>
+    <i class="${rideIcon}" aria-hidden="true"></i>
+    Ride the ${ routeName ?? routNumber} 
+    for ${duration} minutes.
+  </li>
+  `
+}
+
+const walkHtml = (walkingIcon, segment) => {
+  const { duration, stopName, stopNumber } = tripPlansObj(segment);
+  if (stopNumber ?? 'destination' !== 'destination') {
+    return `
+    <li>
+      <i class="${walkingIcon}" aria-hidden="true"></i>
+      Walk for ${duration === 1 ? `${duration} minute` : `${duration} minutes`} 
+      to stop #${stopNumber ?? 'destination'} - ${stopName ?? 'destination'}
+    </li>
+    `
+  }
+  return `
+  <li>
+    <i class="${walkingIcon}" aria-hidden="true"></i>
+    Walk for ${duration === 1 ? `${duration} minute` : `${duration} minutes`}
+    to ${stopName ?? 'destination'}
+  </li>
+  `
+}
+
+const handleEdgeCase = (edgeCase) => {
+  const myTrip = document.querySelector('.my-trip');
+  myTrip.innerHTML = '';
+  myTrip.innerHTML = edgeCase;
+  return myTrip.innerHTML;
+}
+
+const tripPlansObj = (segment) => {
+  const plansObj = {};
+  plansObj.duration = segment.times.durations.total;
+  plansObj.stopName = (((segment ?? {}).to ?? {}).stop ?? {}).name;
+  plansObj.StopNumber = (((segment ?? {}).to ?? {}).stop ?? {}).key;
+  plansObj.routeName = ((segment ?? {}).route ?? {}).name;
+  plansObj.routNumber = ((segment ?? {}).route ?? {}).number;
+  plansObj.fromStopNumber = (((segment ?? {}).from ?? {}).stop ?? {}).key;
+  plansObj.fromStopName = (((segment ?? {}).from ?? {}).stop ?? {}).name;
+  return plansObj;
+}
+
+const renderData = async() => {
   const tripPlansData = await getPlansData();
   console.log(tripPlansData);
-  if (tripPlansData.plans.length === 0) return document.querySelector('.my-trip').innerHTML = 'TIME OT TAKE A CAB';
-  tripPlansData.plans.forEach(plan => {
-    renderTransitHtml(plan);
+  if (tripPlansData.plans.length === 0) return handleEdgeCase('TIME OT TAKE A CAB');
+  const myTrip = document.querySelector('.my-trip');
+  myTrip.innerHTML = '';
+  tripPlansData.plans.forEach((plan, index) => {
+    index = insertSegmentHtml(myTrip, index);
+    renderTransitHtml(plan, myTrip);
   });
 }
-
-/*
-plansObj = {
-WalkDuration: segment.times.durations.total,
-stopNumber: (((segment || {}).to || {}).stop || {}).key?? 'destination',
-stopName: (((segment || {}).to || {}).stop || {}).name?? 'destination',
-} 
-
-plansObj = {
-route: ((segment || {}).route || {}).name?? ((segment || {}).route || {}).number,
-rideDuration: segment.times.durations.total,
-} 
-
-plansObj = {
-fromStopNumber: segment.from.stop.key,
-fromStopName: segment.from.stop.name,
-toStopNumber: (segment.to.stop || {}).key,
-toStopName: (segment.to.stop || {}).name,
-}
-*/ 
-/*const checkSelected = () => {
-  const originsLists = document.querySelectorAll('.origins li');
-  const destinationsLists = document.querySelectorAll('.destinations li');
-  const originSelected = originsLists.forEach(li => {
-    li.classList.contains('selected');
-  });
-  if (!originSelected) return document.querySelector('.my-trip').innerHTML = 'Select origin and destination';
-}*/
 
 const planTripButton = document.querySelector('.plan-trip');
 
-const handleButton = (e) => {
-  const eventTarget = e.target;
-  //checkSelected();
-  generateTripPlansArray();
+const handleButton = () => {
+  renderData();
 }
 
 planTripButton.addEventListener('click', handleButton);
